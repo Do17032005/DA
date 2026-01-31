64e3823b4850b342dffef86d2b5802aaec04733b
Rumnn <nguyentiendoanh2k5@gmail.com>
2026-01-31 10:00:27 +0700

    upload project

---
+++ src/main/java/com/clothes/clothesApplication.java
@@ -0,0 +1,11 @@
+package com.clothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class clothesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(clothesApplication.class, args);
+    }
+}
